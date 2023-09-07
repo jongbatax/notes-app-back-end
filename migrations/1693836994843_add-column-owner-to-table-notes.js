45dae66ee60130371b0dc400a59817960dccff03
jongbatax <herry.it.2007@gmail.com>
2023-09-07 20:05:48 +0700

    authorization implemented

---
+++ migrations/1693836994843_add-column-owner-to-table-notes.js
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+
+
+exports.up = pgm => {
+  pgm.addColumn('notes', {
+    owner: {
+      type: 'VARCHAR(50)',
+    }
+  });
+};
+
+exports.down = pgm => {
+  pgm.dropColumn('notes', 'owner')
+};
